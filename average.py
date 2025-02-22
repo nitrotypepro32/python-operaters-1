c0575a41b401d4f3b3263da05a2f1abc7ba2b30a
nitrotypepro32 <pskreddy@gmail.com>
2025-02-21 19:55:35 -0600

    Add files via upload

---
+++ average.py
@@ -0,0 +1,17 @@
+tree1=87
+tree2=98
+tree3=64
+tree4=32   
+tree5=65
+tree6=87
+tree8=98
+tree9=23
+tree10=45
+tree11=67   
+tree12=89
+tree13=45
+tree7=139
+tree14=67         
+average = (tree1 + tree2 + tree3 + tree4 + tree5 + tree6 + tree7 + tree8 + tree9 + tree10 + tree11 + tree12 + tree13 + tree14) / 14
+print ("The average height of all the trees is: ", average)
+
---
+++ percentile.py
@@ -0,0 +1,14 @@
+reading=78
+writing=85 
+math=100
+science=95
+social_studies=89
+sum = reading + writing + math + science + social_studies
+average = sum / 5
+print ("The average score of all the subjects is: ", average)
+percentage = (sum / 500) * 100
+print ("The percentage of all the subjects is: ", percentage)
+
+
+
+
